1e4e309c6b1625a47e13921d0e99d0b5a74aefac
Soubhik <soubhikgit456@gmail.com>
2026-03-28 11:25:05 +0530

    add Q3 file

---
+++ Day1/Q3.java
@@ -0,0 +1,10 @@
+public class Q3{
+    public static void main(String[] args) {
+        int n = 4;
+       
+        for(int i = n;i>0;i--){
+            System.out.println(i);
+            
+        }
+    }
+}
